338600a2d04bbca225b155929a37d2479bff57e3
SathvikY3533 <sathvik.y007@gmail.com>
2026-01-23 16:46:46 -0600

    code shenanigins

---
+++ firmware/build/stm32g4xx_hal_smartcard_ex.d
@@ -0,0 +1,100 @@
+/home/pani_/LHRS/VCAT-Dashboard/firmware/test/../build/stm32g4xx_hal_smartcard_ex.o: \
+ stm/stm32g4xx/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_smartcard_ex.c \
+ stm/stm32g4xx/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal.h \
+ common/Inc/stm32g4xx_hal_conf.h \
+ stm/stm32g4xx/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_rcc.h \
+ stm/stm32g4xx/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_def.h \
+ stm/stm32g4xx/CMSIS/Device/ST/STM32G4xx/Include/stm32g4xx.h \
+ stm/stm32g4xx/CMSIS/Device/ST/STM32G4xx/Include/stm32g473xx.h \
+ stm/stm32g4xx/CMSIS/Include/core_cm4.h \
+ stm/stm32g4xx/CMSIS/Include/cmsis_version.h \
+ stm/stm32g4xx/CMSIS/Include/cmsis_compiler.h \
+ stm/stm32g4xx/CMSIS/Include/cmsis_gcc.h \
+ stm/stm32g4xx/CMSIS/Include/mpu_armv7.h \
+ stm/stm32g4xx/CMSIS/Device/ST/STM32G4xx/Include/system_stm32g4xx.h \
+ stm/stm32g4xx/STM32G4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ stm/stm32g4xx/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_rcc_ex.h \
+ stm/stm32g4xx/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_gpio.h \
+ stm/stm32g4xx/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_gpio_ex.h \
+ stm/stm32g4xx/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_dma.h \
+ stm/stm32g4xx/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_dma_ex.h \
+ stm/stm32g4xx/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_cortex.h \
+ stm/stm32g4xx/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_adc.h \
+ stm/stm32g4xx/STM32G4xx_HAL_Driver/Inc/stm32g4xx_ll_adc.h \
+ stm/stm32g4xx/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_adc_ex.h \
+ stm/stm32g4xx/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_exti.h \
+ stm/stm32g4xx/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_fdcan.h \
+ stm/stm32g4xx/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_flash.h \
+ stm/stm32g4xx/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_flash_ex.h \
+ stm/stm32g4xx/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_flash_ramfunc.h \
+ stm/stm32g4xx/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_iwdg.h \
+ stm/stm32g4xx/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_i2c.h \
+ stm/stm32g4xx/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_i2c_ex.h \
+ stm/stm32g4xx/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_lptim.h \
+ stm/stm32g4xx/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_opamp.h \
+ stm/stm32g4xx/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_opamp_ex.h \
+ stm/stm32g4xx/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_pwr.h \
+ stm/stm32g4xx/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_pwr_ex.h \
+ stm/stm32g4xx/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_qspi.h \
+ stm/stm32g4xx/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_smartcard.h \
+ stm/stm32g4xx/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_smartcard_ex.h \
+ stm/stm32g4xx/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_smbus.h \
+ stm/stm32g4xx/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_smbus_ex.h \
+ stm/stm32g4xx/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_spi.h \
+ stm/stm32g4xx/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_spi_ex.h \
+ stm/stm32g4xx/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_tim.h \
+ stm/stm32g4xx/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_tim_ex.h \
+ stm/stm32g4xx/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_uart.h \
+ stm/stm32g4xx/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_uart_ex.h \
+ stm/stm32g4xx/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_usart.h \
+ stm/stm32g4xx/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_usart_ex.h \
+ stm/stm32g4xx/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_wwdg.h
+stm/stm32g4xx/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal.h:
+common/Inc/stm32g4xx_hal_conf.h:
+stm/stm32g4xx/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_rcc.h:
+stm/stm32g4xx/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_def.h:
+stm/stm32g4xx/CMSIS/Device/ST/STM32G4xx/Include/stm32g4xx.h:
+stm/stm32g4xx/CMSIS/Device/ST/STM32G4xx/Include/stm32g473xx.h:
+stm/stm32g4xx/CMSIS/Include/core_cm4.h:
+stm/stm32g4xx/CMSIS/Include/cmsis_version.h:
+stm/stm32g4xx/CMSIS/Include/cmsis_compiler.h:
+stm/stm32g4xx/CMSIS/Include/cmsis_gcc.h:
+stm/stm32g4xx/CMSIS/Include/mpu_armv7.h:
+stm/stm32g4xx/CMSIS/Device/ST/STM32G4xx/Include/system_stm32g4xx.h:
+stm/stm32g4xx/STM32G4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+stm/stm32g4xx/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_rcc_ex.h:
+stm/stm32g4xx/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_gpio.h:
+stm/stm32g4xx/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_gpio_ex.h:
+stm/stm32g4xx/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_dma.h:
+stm/stm32g4xx/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_dma_ex.h:
+stm/stm32g4xx/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_cortex.h:
+stm/stm32g4xx/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_adc.h:
+stm/stm32g4xx/STM32G4xx_HAL_Driver/Inc/stm32g4xx_ll_adc.h:
+stm/stm32g4xx/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_adc_ex.h:
+stm/stm32g4xx/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_exti.h:
+stm/stm32g4xx/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_fdcan.h:
+stm/stm32g4xx/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_flash.h:
+stm/stm32g4xx/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_flash_ex.h:
+stm/stm32g4xx/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_flash_ramfunc.h:
+stm/stm32g4xx/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_iwdg.h:
+stm/stm32g4xx/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_i2c.h:
+stm/stm32g4xx/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_i2c_ex.h:
+stm/stm32g4xx/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_lptim.h:
+stm/stm32g4xx/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_opamp.h:
+stm/stm32g4xx/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_opamp_ex.h:
+stm/stm32g4xx/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_pwr.h:
+stm/stm32g4xx/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_pwr_ex.h:
+stm/stm32g4xx/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_qspi.h:
+stm/stm32g4xx/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_smartcard.h:
+stm/stm32g4xx/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_smartcard_ex.h:
+stm/stm32g4xx/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_smbus.h:
+stm/stm32g4xx/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_smbus_ex.h:
+stm/stm32g4xx/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_spi.h:
+stm/stm32g4xx/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_spi_ex.h:
+stm/stm32g4xx/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_tim.h:
+stm/stm32g4xx/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_tim_ex.h:
+stm/stm32g4xx/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_uart.h:
+stm/stm32g4xx/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_uart_ex.h:
+stm/stm32g4xx/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_usart.h:
+stm/stm32g4xx/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_usart_ex.h:
+stm/stm32g4xx/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_wwdg.h:
---
+++ firmware/build/stm32g4xx_ll_rtc.d
@@ -0,0 +1,2 @@
+/home/pani_/LHRS/VCAT-Dashboard/firmware/test/../build/stm32g4xx_ll_rtc.o: \
+ stm/stm32g4xx/STM32G4xx_HAL_Driver/Src/stm32g4xx_ll_rtc.c
