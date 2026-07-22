de4ac7b9d6fee6b03e24e9b6350d897d8861fe3b
SathvikY3533 <sathvik.y007@gmail.com>
2026-07-22 16:15:32 -0500

    im stopid?

--- firmware/tasks/Src/Task_Send_Switch_States.c
+++ firmware/tasks/Src/Task_Send_Switch_States.c
@@ -32,7 +32,7 @@ void Task_Send_Switch_States(void *argument) {
 	uint32_t reset_gear = 0;
 	uint32_t bitmap = 0;
 	uint32_t previous_bitmap = 0;
-	uint32_t steering_angle_reset = 0;
+	//uint32_t steering_angle_reset = 0;
 
 	while (1) {
 		previous_bitmap = bitmap;
@@ -61,23 +61,23 @@ void Task_Send_Switch_States(void *argument) {
 			bitmap |= (0x1 << SW_NEUTRAL_GEAR);
 		}
 
-		if((((bitmap >> SW_REGEN_ENABLE) & 0x1) == 1) && (((bitmap >> SW_CRUISE_ENABLE) & 0x1) == 1)) {
-			steering_angle_reset += 1;
-		} else {
-			steering_angle_reset = 0;
-		}
+		//if((((bitmap >> SW_REGEN_ENABLE) & 0x1) == 1) && (((bitmap >> SW_CRUISE_ENABLE) & 0x1) == 1)) {
+		//	steering_angle_reset += 1;
+		//} else {
+		//	steering_angle_reset = 0;
+		//}
 
-		if(steering_angle_reset >= 10 && ((bitmap >> SW_REV) & 0x1) != 1) {
-			if(SteeringCAN_ResetAngle(SEND_SWITCH_STATES_TASK_DELAY_TICKS) != CAN_OK) {
-				printf("SteeringCAN_ResetAngle failed to send\r\n");
-			}
-			steering_angle_reset = 0;
-		} else if(steering_angle_reset >= 10 && ((bitmap >> SW_REV) & 0x1) == 1) {
-			if(SteeringCAN_ResetCalibration(SEND_SWITCH_STATES_TASK_DELAY_TICKS) != CAN_OK) {
-				printf("SteeringCAN_ResetCalibration failed to send\r\n");
-			}
-			steering_angle_reset = 0;
-		}
+		//if(steering_angle_reset >= 10 && ((bitmap >> SW_REV) & 0x1) != 1) {
+		//	if(SteeringCAN_ResetAngle(SEND_SWITCH_STATES_TASK_DELAY_TICKS) != CAN_OK) {
+		//		printf("SteeringCAN_ResetAngle failed to send\r\n");
+		//	}
+		//	steering_angle_reset = 0;
+		//} else if(steering_angle_reset >= 10 && ((bitmap >> SW_REV) & 0x1) == 1) {
+		//	if(SteeringCAN_ResetCalibration(SEND_SWITCH_STATES_TASK_DELAY_TICKS) != CAN_OK) {
+		//		printf("SteeringCAN_ResetCalibration failed to send\r\n");
+		//	}
+		//	steering_angle_reset = 0;
+		//}
 
 		if((bitmap >> SW_IGN_MTR) & 0x1) bitmap |= (0x1 << SW_IGN_ARR);
 
